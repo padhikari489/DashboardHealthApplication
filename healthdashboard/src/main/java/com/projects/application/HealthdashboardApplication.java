798663183b836af4d3ba637a0e32d75dbfb846cd
pawan489 <paone.adhikary@gmail.com>
2023-02-27 17:02:17 -0600

    Initial Commit

---
+++ healthdashboard/src/main/java/com/projects/application/HealthdashboardApplication.java
@@ -0,0 +1,14 @@
+package com.projects.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthdashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthdashboardApplication.class, args);
+		System.out.println("Application started...");
+	}
+
+}
